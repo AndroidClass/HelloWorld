4aa9ed62c8794e3080542bf34ab30f0658907135
sky <1005672068@qq.com>
2016-06-15 10:23:47 +0800

    change Hello.java

--- Hello.java
+++ Hello.java
@@ -1,11 +1,8 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello World!");
-		System.out.println("Hello China!");
-		System.out.println("Hello girls!");
-		
-		
+		System.out.println("Hello World!");	
 		
 		System.out.println("版本升级，马上更新");
+		System.out.println("添加新功能，应用批量更新");
 	}
 }
