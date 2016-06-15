026c62f7052a068c21d31b4cbfde3a3237685fee
sky <1005672068@qq.com>
2016-06-15 10:25:43 +0800

    change Hello.java file

--- Hello.java
+++ Hello.java
@@ -1,3 +1,4 @@
+import java.utils.*;
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
@@ -7,5 +8,12 @@ public class Hello{
 		
 		
 		System.out.println("版本升级，马上更新");
+		
+		random();
+	}
+	
+	public static void random(){
+		Random random = new Random();
+		System.out.println("生成的随机数为：" + random.nextInt());
 	}
 }
