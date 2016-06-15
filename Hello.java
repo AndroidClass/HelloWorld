c8151a44e99d940a4c6843c07916ff2239a10ed6
sky <1005672068@qq.com>
2016-06-15 09:38:55 +0800

    update version

--- Hello.java
+++ Hello.java
@@ -3,5 +3,9 @@ public class Hello{
 		System.out.println("Hello World!");
 		System.out.println("Hello China!");
 		System.out.println("Hello girls!");
+		
+		
+		
+		System.out.println("版本升级，马上更新");
 	}
 }
--- hello/Hello.java
+++ hello/Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello World!");
-		System.out.println("Hello China!");
 		System.out.println("Hello girls!");
+		
+		System.out.println("版本升级，马上更新");
 	}
 }
